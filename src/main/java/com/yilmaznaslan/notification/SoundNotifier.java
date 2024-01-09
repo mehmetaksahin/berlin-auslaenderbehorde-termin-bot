e50d09afc5030b629c624eafdad8cd3d8d9ecdef
Mehmet Akşahin <muh.mehmet.aksahin@gmail.com>
2024-01-10 01:19:42 +0300

    added alert message

--- src/main/java/com/yilmaznaslan/notification/SoundNotifier.java
+++ src/main/java/com/yilmaznaslan/notification/SoundNotifier.java
@@ -8,21 +8,37 @@ import java.io.IOException;
 public class SoundNotifier implements NotificationAdapter {
     private static final Logger logger = LoggerFactory.getLogger(SoundNotifier.class);
 
+    private static final String ALERT_MESSAGE = "Alert! Attention required! Aksahin Hukuk Bürosu!";
+
     @Override
     public void triggerNotification(String message) {
         String osName = System.getProperty("os.name").toLowerCase();
         String osVersion = System.getProperty("os.version").toLowerCase();
+
+        try {
+            int i = 0;
+            while (i < 10) {
+                seslendir(osName, osVersion);
+                i++;
+                Thread.sleep(5000);
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void seslendir(String osName, String osVersion) {
         try {
             if (osName.contains("win")) {
                 String[] cmd = {
                         "powershell",
-                        "Add-Type -AssemblyName System.Speech; $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer; $speak.Speak('Alert! Attention required! Aksahin Hukuk - Muslum Selim Aksahin cok karizma birisi - Muslum Selim Aksahin is clever and handsome');"
+                        "Add-Type -AssemblyName System.Speech; $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer; $speak.Speak('" + ALERT_MESSAGE + "');"
                 };
                 Runtime.getRuntime().exec(cmd);
             } else if (osName.contains("mac")) {
-                Runtime.getRuntime().exec(new String[]{"say", "Alert! Attention required! Aksahin Hukuk - Muslum Selim Aksahin cok karizma birisi - Muslum Selim Aksahin is clever and handsome"});
+                Runtime.getRuntime().exec(new String[]{"say", ALERT_MESSAGE});
             } else if (osVersion.contains("wsl")) {
-                Runtime.getRuntime().exec(new String[]{"/bin/sh", "-c", "powershell.exe -c \"(New-Object -ComObject SAPI.SpVoice).Speak('Alert! Attention required! Aksahin Hukuk - Muslum Selim Aksahin cok karizma birisi - Muslum Selim Aksahin is clever and handsome')\"\n"});
+                Runtime.getRuntime().exec(new String[]{"/bin/sh", "-c", "powershell.exe -c \"(New-Object -ComObject SAPI.SpVoice).Speak('" + ALERT_MESSAGE + "')\"\n"});
             } else {
                 // Handle other operating systems
             }
@@ -30,4 +46,5 @@ public class SoundNotifier implements NotificationAdapter {
             logger.error("Failed to notify. Reason: ", e);
         }
     }
+
 }
